b6f7292d6bb3cfeb91682b6a06f6955588a7152d
ismael10 <isomael10@gmail.com>
2025-01-19 23:08:36 +0000

    main

--- dons.js
+++ dons.js
@@ -17,3 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 5000);
     });
 });
+
+// Sélectionne le bouton "menu-toggle" et la liste du menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.navbar nav ul');
+
+// Ajoute un événement au clic sur le bouton hamburger
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); // Ajoute ou retire la classe "active" pour afficher/masquer le menu
+});
+
